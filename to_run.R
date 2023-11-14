865bb04f93d3c1151bf34fd6f5170052a823297a
Davide <davide.messina@ars.toscana.it>
2023-11-14 10:39:02 +0100

    Added comment with link to server

--- to_run.R
+++ to_run.R
@@ -6,6 +6,7 @@ generate_Rmd()
 generate_website()
 
 # build the local site (that can be browsed immediately from ./g_website/static/diagram_draft.html)
+# Site served http://localhost:4321
 serve_site()
 
 # commit updated DAG, index and codebooks (after the committed changes are pushed, the site will be updated by GitHub within some minutes)
